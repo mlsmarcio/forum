0fd1fb52515d020a45240df5d1be0e56d2783f2e
mlsmarcio <mlsmarciogm@gmail.com>
2023-06-28 22:26:02 -0300

    Voltando para um commit anterior para trocar o bd h2 pelo mysql

--- src/main/java/br/com/alura/forum/modelo/Curso.java
+++ src/main/java/br/com/alura/forum/modelo/Curso.java
@@ -12,9 +12,8 @@ public class Curso {
 	private String nome;
 	private String categoria;
 	
-	public Curso() {
-	}
-
+	public Curso() {}
+	
 	public Curso(String nome, String categoria) {
 		this.nome = nome;
 		this.categoria = categoria;

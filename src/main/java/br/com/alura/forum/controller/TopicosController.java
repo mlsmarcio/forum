d826df8e9aa1a72a91460e23fe45bef4d19e0aed
mlsmarcio <mlsmarciogm@gmail.com>
2023-06-29 22:51:59 -0300

    implementando o cadastro de topicos através da API REST e retornando o código 201 com a representação do objeto criado e o cabeçalho http chamado location com a url

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,27 +1,61 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
+	
+	@Autowired
+	private CursoRepository cursoRepository;
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")
-	public List<TopicoDto> lista() {
+	@GetMapping
+	public List<TopicoDto> lista(String nomeCurso) {
 		
-		List<Topico> topicos = topicoRepository.findAll();
+		System.out.println(nomeCurso);
+		
+		// FILTRANDO PELO TITULO - PRECISA CRIAR O MÉTODO NO REPOSITORY APENAS A ASSINATURA
+		// List<Topico> topicos = topicoRepository.findByTitulo(nomeTitulo);
+		
+		List<Topico> topicos;
+		
+		if (nomeCurso == null) {
+			topicos = topicoRepository.findAll();
+		} else {
+			topicos = topicoRepository.obterPorNomeDoCurso(nomeCurso); // findByCurso_Nome(nomeCurso);
+		}
 		return TopicoDto.converter(topicos);
+	}
+	
+	// Retorna um ResponseEntity para devolver o código 201 + a uri + a representação do que foi criado
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+		
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+		
 	}
 }
--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,21 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
+	
+	// MESMA CONSULTA UTILIZANDO O JPQL 
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	List<Topico> obterPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+	
+	
 
 }

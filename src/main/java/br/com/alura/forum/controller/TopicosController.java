aa873ac2de4923b24255e3a782d8fa304168af7e
mlsmarcio <mlsmarciogm@gmail.com>
2023-07-04 23:01:35 -0300

    Melhorando o código da paginação com o spring data, passando apenas um parametro Pageable que receberá os dados da paginacao da url. Especificando a paginacao/ordenacao padrao

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,15 +44,9 @@ public class TopicosController {
 	private TopicoRepository topicoRepository;
 
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
-			@RequestParam int pagina, @RequestParam int qtd) {
-		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
-		// FILTRANDO PELO TITULO - PRECISA CRIAR O MÉTODO NO REPOSITORY APENAS A
-		// ASSINATURA
-		// List<Topico> topicos = topicoRepository.findByTitulo(nomeTitulo);
-		
 		Page<Topico> topicos = null;
 		
 		if (nomeCurso == null) {

f46ea3bb3535160531932b94d8453b81e00cdf63
mlsmarcio <mlsmarciogm@gmail.com>
2023-07-18 22:52:23 -0300

    Retornando o token e o tipo de autenticação em formato json para o cliente utilizar na próxima autenticação

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.config.security.TokenService;
+import br.com.alura.forum.controller.dto.TokenDto;
 import br.com.alura.forum.controller.form.LoginForm;
 
 @RestController
@@ -27,7 +28,7 @@ public class AutenticacaoController {
 	private TokenService tokenService;
 	
 	@PostMapping
-	public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form) {
+	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
 		
 		// CLIENTE MANDA USUÁRIO E A SENHA
 		UsernamePasswordAuthenticationToken dadosLogin = form.converter() ;
@@ -39,9 +40,7 @@ public class AutenticacaoController {
 			// GERANDO O TOKEN
 			String token = tokenService.gerarToken(authentication);
 			
-			System.out.println(token);
-			
-			return ResponseEntity.ok().build();
+			return ResponseEntity.ok(new TokenDto(token, "Bearer")); //Beure
 			
 		} catch (AuthenticationException e) {
 			return ResponseEntity.badRequest().build();

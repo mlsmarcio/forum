bd685322160ef5cf83ac08d346e9f05c6c912a88
mlsmarcio <mlsmarciogm@gmail.com>
2023-08-02 22:54:55 -0300

    Adicionando o actuator para monitoramento da aplicacao

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -48,6 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 			.antMatchers(HttpMethod.POST, "/auth").permitAll()
+			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 			.anyRequest().authenticated()
 			.and().csrf().disable()  //tipo de ataque racker 
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // NÃO USARÁ SESSÃO

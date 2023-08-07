a2e2175d56a6eec9abf243cf5ed872d1a2640dd1
mlsmarcio <mlsmarciogm@gmail.com>
2023-08-06 21:53:24 -0300

    Adicionando monitoramento com o Spring Boot Actuator e adicionando o spring boot admin client para que ele possa monitorar a API

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,7 +2,6 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
